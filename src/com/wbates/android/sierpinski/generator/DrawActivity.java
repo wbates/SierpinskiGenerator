5335a664fdd49d50348fc19539ec8669316fc6cd
Bill Bates <wbates@wbates.com>
2012-07-23 00:30:17 -0400

    sharing menu framework initialized

--- src/com/wbates/android/sierpinski/generator/DrawActivity.java
+++ src/com/wbates/android/sierpinski/generator/DrawActivity.java
@@ -2,12 +2,16 @@ package com.wbates.android.sierpinski.generator;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 // TODO add menu options
+import android.widget.Toast;
 
 public class DrawActivity extends Activity {
 
@@ -34,4 +38,33 @@ public class DrawActivity extends Activity {
     main.addView(new SierpinskiCanvas(this, sb1x,sb1y,sb2x,sb2y,sb3x,sb3y));
   }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    menu.add(1,1,0,R.string.menu_save_image);
+    menu.add(1,2,1,R.string.menu_share_fb);
+    return true;
+  }
+  
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    Context context = getApplicationContext();
+    CharSequence text = "";
+    int duration = Toast.LENGTH_SHORT;
+    Toast toast;
+
+    switch(item.getItemId()) {
+    case 1:  text = "Future feature: Save as PNG image in Gallery";
+                                toast = Toast.makeText(context, text, duration);
+                                toast.show();
+                                return true;
+    case 2:    text = "Future feature: Share image to Facebook";
+                                toast = Toast.makeText(context, text, duration);
+                                toast.show();
+                                return true;
+    }
+    
+    return false;
+  }
 }
