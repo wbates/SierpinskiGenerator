af6949e13dcfe46ab41ff9a0bbabb56fad03141e
Bill Bates <wbates@wbates.com>
2012-07-22 23:17:32 -0400

    removed inefficencies in MainActivity

--- src/com/wbates/android/sierpinski/generator/MainActivity.java
+++ src/com/wbates/android/sierpinski/generator/MainActivity.java
@@ -15,10 +15,11 @@ import android.util.DisplayMetrics;
 
 public class MainActivity extends Activity implements OnSeekBarChangeListener {
 
-  SeekBar sb1x,sb1y,sb2x,sb2y,sb3x,sb3y;
-  TextView tv1x,tv1y,tv2x,tv2y,tv3x,tv3y;
+  SeekBar sb[][] = new SeekBar[2][3];
+  TextView tv[][] = new TextView[2][3];
+  String dimlabel[] = {"x","y"};
   Button btngen;
-  int height,width;
+  int dimsize[] = {0,0};
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -28,49 +29,48 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
     DisplayMetrics displaymetrics = new DisplayMetrics();
     getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
 
-    height = displaymetrics.heightPixels;
-    width = displaymetrics.widthPixels;
+    dimsize[0] = displaymetrics.heightPixels;
+    dimsize[1] = displaymetrics.widthPixels;
 
-    sb1x = (SeekBar) findViewById(R.id.sb1x);
-    sb1y = (SeekBar) findViewById(R.id.sb1y);
-    sb1x.setOnSeekBarChangeListener(this);
-    sb1y.setOnSeekBarChangeListener(this);
-
-    sb2x = (SeekBar) findViewById(R.id.sb2x);
-    sb2y = (SeekBar) findViewById(R.id.sb2y);
-    sb2x.setOnSeekBarChangeListener(this);
-    sb2y.setOnSeekBarChangeListener(this);
-
-    sb3x = (SeekBar) findViewById(R.id.sb3x);
-    sb3y = (SeekBar) findViewById(R.id.sb3y);
-    sb3x.setOnSeekBarChangeListener(this);
-    sb3y.setOnSeekBarChangeListener(this);
-
-    tv1x = (TextView) findViewById(R.id.tv1x);
-    tv1y = (TextView) findViewById(R.id.tv1y);    
-    tv1x.setText("Point 1 X Value: " + progressToDim(sb1x.getProgress(),width));
-    tv1y.setText("Point 1 Y Value: " + progressToDim(sb1y.getProgress(),height));
-
-    tv2x = (TextView) findViewById(R.id.tv2x);
-    tv2y = (TextView) findViewById(R.id.tv2y);    
-    tv2x.setText("Point 2 X Value: " + progressToDim(sb2x.getProgress(),width));
-    tv2y.setText("Point 2 Y Value: " + progressToDim(sb2y.getProgress(),height));
-
-    tv3x = (TextView) findViewById(R.id.tv3x);
-    tv3y = (TextView) findViewById(R.id.tv3y);    
-    tv3x.setText("Point 3 X Value: " + progressToDim(sb3x.getProgress(),width));
-    tv3y.setText("Point 3 Y Value: " + progressToDim(sb3y.getProgress(),height));
-
+    for(int dimension=0;dimension<2;dimension++) {
+      for(int idval=0;idval<3;idval++) {
+        int id = 0;
+        try {
+          id = (Integer) R.id.class.getField("sb"+(idval+1)+dimlabel[dimension]).get(null);
+          //sbid[dimension][idval] = id;
+          sb[dimension][idval] = (SeekBar) findViewById(id);
+          sb[dimension][idval].setOnSeekBarChangeListener(this);
+          
+          id = (Integer) R.id.class.getField("tv"+(idval+1)+dimlabel[dimension]).get(null);
+          tv[dimension][idval] = (TextView) findViewById(id);
+          tv[dimension][idval].setText("Point " + idval + " " + dimlabel[dimension] + " Value: " + 
+              progressToDim(sb[dimension][idval].getProgress(),dimsize[dimension]));
+          
+        } catch (IllegalArgumentException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        } catch (SecurityException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        } catch (IllegalAccessException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+    }
+    
     btngen = (Button) findViewById(R.id.btngen);
     btngen.setOnClickListener(new OnClickListener() {
       public void onClick(View v) {
         Intent i = new Intent(MainActivity.this, DrawActivity.class);
-        i.putExtra("sb1x", progressToDim(sb1x.getProgress(),width));
-        i.putExtra("sb1y", progressToDim(sb1y.getProgress(),height));
-        i.putExtra("sb2x", progressToDim(sb2x.getProgress(),width));
-        i.putExtra("sb2y", progressToDim(sb2y.getProgress(),height));
-        i.putExtra("sb3x", progressToDim(sb3x.getProgress(),width));
-        i.putExtra("sb3y", progressToDim(sb3y.getProgress(),height));
+        for(int dimension=0;dimension<2;dimension++) {
+          for(int idval=0;idval<3;idval++) {
+            i.putExtra("sb"+(idval+1)+dimlabel[dimension], progressToDim(sb[dimension][idval].getProgress(),dimsize[dimension]));
+          }
+        }
         Context context = getApplicationContext();
         CharSequence text = "Generating Sierpinski ... patience if it takes a while to see something.";
         int duration = Toast.LENGTH_SHORT;
@@ -86,21 +86,21 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener {
   @Override
   public void onProgressChanged(SeekBar bar, int progress, boolean fromUser)
   {
-    int progress_w = progressToDim(progress,width);
-    int progress_h = progressToDim(progress,height);
+    int dimprogress[] = {progressToDim(progress,dimsize[0]),progressToDim(progress,dimsize[1])};
 
+    // TODO change this to more optimized logic
     switch(bar.getId()) {
-    case R.id.sb1x: tv1x.setText("Point 1 X Value: " + progress_w);
+    case R.id.sb1x: tv[0][0].setText("Point 1 X Value: " + dimprogress[0]);
     break;
-    case R.id.sb1y: tv1y.setText("Point 1 Y Value: " + progress_h);
+    case R.id.sb1y: tv[1][0].setText("Point 1 Y Value: " + dimprogress[1]);
     break;
-    case R.id.sb2x: tv2x.setText("Point 2 X Value: " + progress_w);
+    case R.id.sb2x: tv[0][1].setText("Point 2 X Value: " + dimprogress[0]);
     break;
-    case R.id.sb2y: tv2y.setText("Point 2 Y Value: " + progress_h);
+    case R.id.sb2y: tv[1][1].setText("Point 2 Y Value: " + dimprogress[1]);
     break;
-    case R.id.sb3x: tv3x.setText("Point 3 X Value: " + progress_w);
+    case R.id.sb3x: tv[0][2].setText("Point 3 X Value: " + dimprogress[0]);
     break;
-    case R.id.sb3y: tv3y.setText("Point 3 Y Value: " + progress_h);
+    case R.id.sb3y: tv[1][2].setText("Point 3 Y Value: " + dimprogress[1]);
     break;
     default:        break;
     }
