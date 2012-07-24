726d5c6688834ea55658d1e5ff42852e3d39bf06
Bill Bates <wbates@wbates.com>
2012-07-24 09:17:53 -0400

    added basic save to image functionality

--- src/com/wbates/android/sierpinski/generator/DrawActivity.java
+++ src/com/wbates/android/sierpinski/generator/DrawActivity.java
@@ -1,9 +1,14 @@
 package com.wbates.android.sierpinski.generator;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
@@ -23,20 +28,22 @@ public class DrawActivity extends Activity {
     super.onCreate(savedInstanceState);
     requestWindowFeature(Window.FEATURE_NO_TITLE);
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
     setContentView(R.layout.activity_draw);
 
     Intent i = getIntent();
-    // TODO consider removing these lines of code and accessing the intents directly in SierpinskiCanvas call
-    int sb1x = i.getIntExtra("sb1x",0);
-    int sb1y = i.getIntExtra("sb1y",0);
-    int sb2x = i.getIntExtra("sb2x",0);
-    int sb2y = i.getIntExtra("sb2y",0);
-    int sb3x = i.getIntExtra("sb3x",0);
-    int sb3y = i.getIntExtra("sb3y",0);
-    
+    canvas = new SierpinskiCanvas(
+        this,
+        i.getIntExtra("sb1x",0),
+        i.getIntExtra("sb1y",0),
+        i.getIntExtra("sb2x",0),
+        i.getIntExtra("sb2y",0),
+        i.getIntExtra("sb3x",0),
+        i.getIntExtra("sb3y",0)
+        );
+
     FrameLayout main = (FrameLayout) findViewById(R.id.sierpinski_draw_view);
-    main.addView(new SierpinskiCanvas(this, sb1x,sb1y,sb2x,sb2y,sb3x,sb3y));
+    main.addView(canvas);
   }
 
   @Override
@@ -46,7 +53,7 @@ public class DrawActivity extends Activity {
     menu.add(1,2,1,R.string.menu_share_fb);
     return true;
   }
-  
+
   @Override
   public boolean onOptionsItemSelected(MenuItem item)
   {
@@ -56,16 +63,46 @@ public class DrawActivity extends Activity {
     Toast toast;
 
     switch(item.getItemId()) {
-    case 1:  text = "Future feature: Save as PNG image in Gallery";
-                                toast = Toast.makeText(context, text, duration);
-                                toast.show();
-                                return true;
-    case 2:    text = "Future feature: Share image to Facebook";
-                                toast = Toast.makeText(context, text, duration);
-                                toast.show();
-                                return true;
+    case 1:  saveToImage();
+    return true;
+    case 2:  text = "Future feature: Share image to Facebook";
+    toast = Toast.makeText(context, text, duration);
+    toast.show();
+    return true;
+    }
+
+    return false;
+  }
+
+  public void saveToImage() {
+    // TODO image saving, but not to gallery
+    Context context = getApplicationContext();
+    Toast toast;
+    int duration = Toast.LENGTH_SHORT;
+    CharSequence text = "";
+
+    File folder = new File(Environment.getExternalStorageDirectory() + "/sierpinski");
+    boolean success = false;
+
+    if (!folder.exists()) {
+      success = folder.mkdir();
+    } else {
+      success = true;
     }
     
-    return false;
+    if (success) {
+      String imagefilename = folder.getAbsolutePath() + "/sier.png";
+      text = "Image saved to " + imagefilename;
+
+      try {
+        canvas.getDrawCache().compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(new File(imagefilename)));
+      } catch (Exception e) {
+        e.printStackTrace();
+      } 
+    } else {
+      text = "Unable to create folder: " + folder.getAbsolutePath(); 
+    }  
+    toast = Toast.makeText(context, text, duration);
+    toast.show();
   }
 }
--- src/com/wbates/android/sierpinski/generator/SierpinskiCanvas.java
+++ src/com/wbates/android/sierpinski/generator/SierpinskiCanvas.java
@@ -5,6 +5,7 @@ import com.wbates.sierpinski.*;
 import android.view.View;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Paint;
 import android.graphics.Canvas;
 
@@ -12,9 +13,12 @@ import android.graphics.Canvas;
 public class SierpinskiCanvas extends View {
   public Sierpinski shape = new Sierpinski();
   private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+  private Bitmap drawcache = null;
 
   public SierpinskiCanvas(Context context, int x1, int y1, int x2, int y2, int x3, int y3) {
     super(context);
+    setDrawingCacheEnabled(true);
+    
     shape.setPoints(new PointCoord(x1,y1), 
         new PointCoord(x2,y2), 
         new PointCoord(x3,y3));
@@ -28,6 +32,10 @@ public class SierpinskiCanvas extends View {
       PointCoord p = shape.getNext();
       canvas.drawPoint((float)p.getX(), (float)p.getY(), mPaint);
     }
-    
+    drawcache = this.getDrawingCache();
+  }
+  
+  public Bitmap getDrawCache() {
+    return drawcache;
   }
 }
