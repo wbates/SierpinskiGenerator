b1aa3f2c608f71fc362bcd0cd479f4c1765d69eb
Bill Bates <wbates@wbates.com>
2012-07-24 23:12:19 -0400

    Added save to gallery functionality, cleaned up image code and moved to separate class

---
+++ src/com/wbates/android/sierpinski/generator/CanvasImageSaver.java
@@ -0,0 +1,102 @@
+package com.wbates.android.sierpinski.generator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
+import android.widget.Toast;
+
+public class CanvasImageSaver {
+  
+  private final String PREF_NAME = "Sierpinski";
+  private SierpinskiCanvas canvas;
+  private Context context;
+  
+  public CanvasImageSaver(SierpinskiCanvas ca, Context co) {
+    canvas = ca;
+    context = co;
+  }
+  
+  public void saveToImage() {
+    Toast toast;
+    int duration = Toast.LENGTH_SHORT;
+    CharSequence text = "";
+
+    File folder = new File(Environment.getExternalStorageDirectory() + "/sierpinski");
+    SharedPreferences settings = context.getSharedPreferences(PREF_NAME, 0);
+    int filecounter = settings.getInt("FILE_COUNTER", 1);
+    boolean success = false;
+    String imagefilename = "sier_" + filecounter;
+    String imagefileextension = "png";
+
+    if (!folder.exists()) {
+      success = folder.mkdir();
+    } else {
+      success = true;
+    }
+    
+    if (success) {
+      String imagefullpathfilename = folder.getAbsolutePath() + "/" + imagefilename + "." + imagefileextension;
+
+      try {
+        canvas.getDrawCache().compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(new File(imagefullpathfilename)));
+      } catch (Exception e) {
+        e.printStackTrace();
+      } 
+      
+      Date date = new Date();
+      text = "Image saved to " + imagefullpathfilename;
+      
+      filecounter++;
+      SharedPreferences.Editor editor = settings.edit();
+      editor.putInt("FILE_COUNTER", filecounter);
+      editor.commit();
+      
+      @SuppressWarnings("unused")
+      Uri u = saveMediaEntry(imagefullpathfilename,imagefilename,"",date.getTime(),0,null);
+    } else {
+      text = "Unable to create folder: " + folder.getAbsolutePath(); 
+    }  
+    toast = Toast.makeText(context, text, duration);
+    toast.show();
+  }
+  
+  private Uri saveMediaEntry(String imagePath,String title,String description,long dateTaken,int orientation,Location loc) {
+    ContentValues v = new ContentValues();
+    v.put(Images.Media.TITLE, title);
+    v.put(Images.Media.DISPLAY_NAME, title);
+    v.put(Images.Media.DESCRIPTION, description);
+    v.put(Images.Media.DATE_ADDED, dateTaken);
+    v.put(Images.Media.DATE_TAKEN, dateTaken);
+    v.put(Images.Media.DATE_MODIFIED, dateTaken) ;
+    v.put(Images.Media.MIME_TYPE, "image/png");
+    v.put(Images.Media.ORIENTATION, orientation);
+
+    File f = new File(imagePath) ;
+    File parent = f.getParentFile() ;
+    String path = parent.toString().toLowerCase() ;
+    String name = parent.getName().toLowerCase() ;
+    v.put(Images.ImageColumns.BUCKET_ID, path.hashCode());
+    v.put(Images.ImageColumns.BUCKET_DISPLAY_NAME, name);
+    v.put(Images.Media.SIZE,f.length()) ;
+    f = null ;
+    
+    if( loc != null ) {
+      v.put(Images.Media.LATITUDE, loc.getLatitude());
+      v.put(Images.Media.LONGITUDE, loc.getLongitude());
+    }
+    v.put("_data",imagePath) ;
+    ContentResolver c = context.getContentResolver() ;
+    return c.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, v);
+  }
+}
--- src/com/wbates/android/sierpinski/generator/DrawActivity.java
+++ src/com/wbates/android/sierpinski/generator/DrawActivity.java
@@ -1,21 +1,15 @@
 package com.wbates.android.sierpinski.generator;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
 import android.os.Bundle;
-import android.os.Environment;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-// TODO add menu options
 import android.widget.Toast;
 
 public class DrawActivity extends Activity {
@@ -63,7 +57,8 @@ public class DrawActivity extends Activity {
     Toast toast;
 
     switch(item.getItemId()) {
-    case 1:  saveToImage();
+    case 1:  CanvasImageSaver imagesaver = new CanvasImageSaver(canvas, context);
+    imagesaver.saveToImage();
     return true;
     case 2:  text = "Future feature: Share image to Facebook";
     toast = Toast.makeText(context, text, duration);
@@ -74,35 +69,4 @@ public class DrawActivity extends Activity {
     return false;
   }
 
-  public void saveToImage() {
-    // TODO image saving, but not to gallery
-    Context context = getApplicationContext();
-    Toast toast;
-    int duration = Toast.LENGTH_SHORT;
-    CharSequence text = "";
-
-    File folder = new File(Environment.getExternalStorageDirectory() + "/sierpinski");
-    boolean success = false;
-
-    if (!folder.exists()) {
-      success = folder.mkdir();
-    } else {
-      success = true;
-    }
-    
-    if (success) {
-      String imagefilename = folder.getAbsolutePath() + "/sier.png";
-      text = "Image saved to " + imagefilename;
-
-      try {
-        canvas.getDrawCache().compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(new File(imagefilename)));
-      } catch (Exception e) {
-        e.printStackTrace();
-      } 
-    } else {
-      text = "Unable to create folder: " + folder.getAbsolutePath(); 
-    }  
-    toast = Toast.makeText(context, text, duration);
-    toast.show();
-  }
 }
