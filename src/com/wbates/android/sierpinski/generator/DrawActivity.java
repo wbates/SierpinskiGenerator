8acbad64d7dfa819a2e43ecc43f9db84b33d7f19
Bill Bates <wbates@wbates.com>
2012-07-26 00:09:33 -0400

    final pre-release commit cleaned up warnings modified version

--- src/com/wbates/android/sierpinski/generator/DrawActivity.java
+++ src/com/wbates/android/sierpinski/generator/DrawActivity.java
@@ -11,7 +11,6 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class DrawActivity extends Activity {
 
